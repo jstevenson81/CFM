2b300fa1e702cf9b16edad79750cdd2bb0e0c8e2
Jonathan Stevenson <jstevenson@retinalscreenings.com>
2017-03-23 15:05:07 -0500

    added the weekly wod view

--- cfm/src/app/wod/wod.component.ts
+++ cfm/src/app/wod/wod.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
 import * as moment from 'moment';
+import * as _ from 'lodash';
 
 import { IProgram } from '../interfaces/IProgram';
 import { IApiWod } from '../interfaces/IApiWod';
@@ -21,12 +22,18 @@ export class WodComponent {
 
   programList: IProgram[];
   wod: IApiWod;
+  weeklyWods: IApiWod[];
   errorMessage: any;
   wodDate: string;
   wodProgramName: string;
+  showWeek: boolean;
 
   constructor(private dataService: WodDataService) {
-    dataService
+    this.showWeek = false;
+  }
+
+  ngOnInit(): void {
+    this.dataService
       .getPrograms()
       .subscribe(
       programs => { this.programList = programs; console.log(this.programList) },
@@ -47,5 +54,33 @@ export class WodComponent {
     // get the data from the wodify API
     this.dataService.getWod(wodDateMoment.format('MM-DD-YYYY'),this.wodProgramName)
       .subscribe(wod => this.wod = wod, error => this.errorMessage = error as any);
+
+    // get the weekly wods
+    this.getWeeklyWods();
+  }
+
+  getWeeklyWods(): void {
+    this.weeklyWods = [];
+    let observables: Array<Observable<IApiWod>> = [];
+    let endDate = moment().endOf('week');
+    let currentDate = moment().startOf('week').add(1, 'day');
+    while (endDate.diff(currentDate) >= 0) {
+      observables.push(this.dataService.getWod(currentDate.format('MM-DD-YYYY'), this.wodProgramName));
+      currentDate = currentDate.add(1, 'day');
+    }
+    
+    // get all the wods for the week
+    Observable.forkJoin(observables)
+    .subscribe(wods=> {
+      _.each(wods, (wod: IApiWod) => {
+        this.weeklyWods.push(wod)
+      });
+      console.log(this.weeklyWods);
+    }, 
+    error => this.errorMessage = error as any);    
+  }
+
+  toggleWeekView(): void {
+    this.showWeek = !this.showWeek;
   }
 }
